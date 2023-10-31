1b8d853ab521fe50b8391f8e73d3ebf2c568495c
Miao Wang <shankerwangmiao@gmail.com>
2023-11-01 01:11:13 +0800

    update renderer so that <tmpl> converts to <pre class="z-tmpl">

--- tuna.py
+++ tuna.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import bs4
 
 
 outdir = Path(os.environ.get('TUNA_TEMP_DIR', './tuna/'))
@@ -7,16 +8,18 @@ os.makedirs(outdir, exist_ok=True)
 
 
 def process(html, config, project, *_):
+    doc = bs4.BeautifulSoup(html, 'html.parser')
+    for tmpl in doc.find_all('tmpl'):
+        tmpl.name = "pre"
+        tmpl.attrs["class"] = tmpl.attrs.get("class", []) + ["z-tmpl"]
+    html = str(doc)
     md = f"""---
 category: help
 layout: help
 mirrorid: {config.get('mirrorid', project)}
 ---
 """ + """
-<!-- 本 markdown 从 tuna/mirrorz-help-ng 自动生成，如需修改请参阅该仓库 -->
-
-<style>.z-help tmpl { display: none }</style>
-
+<!-- 本页面从 tuna/mirrorz-help-ng 自动生成，如需修改请参阅该仓库 -->
 <div class="z-wrap">
     <form class="z-form z-global" onchange="form_update(null)" onsubmit="return false">
         <div>
@@ -28,12 +31,12 @@ mirrorid: {config.get('mirrorid', project)}
             </select>
         </div>
         <div>
-            <input id="144d763c" name="_scheme" type="checkbox" checked>
-            <label for="144d763c">是否使用 HTTPS</label>
+            <input id="a44d763c" name="_scheme" type="checkbox" checked>
+            <label for="a44d763c">是否使用 HTTPS</label>
         </div>
         <div>
-            <input id="4659e7da" name="_sudo" type="checkbox">
-            <label for="4659e7da">是否使用 sudo</label>
+            <input id="a659e7da" name="_sudo" type="checkbox">
+            <label for="a659e7da">是否使用 sudo</label>
         </div>
     </form>
 </div>
@@ -44,6 +47,6 @@ mirrorid: {config.get('mirrorid', project)}
 <script src="/static/js/mustache.min.js?{{ site.data['hash'] }}"></script>
 <script src="/static/js/zdocs.js?{{ site.data['hash'] }}"></script>
 """
-    with open(outdir/f'1970-01-01-{config.get("permalink", project)}.md', 'w') as f:
+    with open(outdir/f'1970-01-01-{config.get("permalink", project)}.html', 'w') as f:
         f.write(md)
     return html
